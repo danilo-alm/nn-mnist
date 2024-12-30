ab158d6f1b46ac69e95c9b902d7da553da86cd59
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-12-30 08:36:43 -0300

    split x and y

--- mnist.cpp
+++ mnist.cpp
@@ -8,38 +8,34 @@ constexpr double EULERS_NUMBER = 2.718281828;
 
 struct MNISTSample {
     int label;
-    std::vector<double> pixels;
+    std::vector<int> pixels;
 };
 
+void load_dataset(const std::string& path, std::vector<std::vector<int>>& x, std::vector<int>& y);
+std::vector<MNISTSample> loadMNISTCSV(const std::string& filename);
+void split_x_and_y(std::vector<MNISTSample>& dataset, std::vector<std::vector<int>>& x, std::vector<int>& y);
 void ReLU(std::vector<double>& arr);
 void softmax(std::vector<double>& arr);
-std::vector<MNISTSample> loadMNISTCSV(const std::string& filename);
 
 int main() {
+    const std::string train_csv = "./MNIST_CSV/mnist_train.csv";
+    const std::string test_csv = "./MNIST_CSV/mnist_test.csv";
+
+    std::vector<std::vector<int>> x_train, x_test;
+    std::vector<int> y_train, y_test;
+
     std::cout << "Reading train dataset" << std::endl;
-    std::vector<MNISTSample> train = loadMNISTCSV("./MNIST_CSV/mnist_train.csv");
+    load_dataset(train_csv, x_train, y_train);
 
     std::cout << "Reading test dataset" << std::endl;
-    std::vector<MNISTSample> test = loadMNISTCSV("./MNIST_CSV/mnist_test.csv");
+    load_dataset(test_csv, x_test, y_test);
 
     return 0;
 }
 
-void ReLU(std::vector<double>& arr) {
-    for (auto& val : arr) {
-        val = std::max(0.0, val);
-    }
-}
-
-void softmax(std::vector<double>& arr) {
-    double sum = 0.0;
-    for (auto& val : arr) {
-        val = std::pow(EULERS_NUMBER, val);
-        sum += val;
-    }
-    for (auto& val : arr) {
-        val /= sum;
-    }
+void load_dataset(const std::string& path, std::vector<std::vector<int>>& x, std::vector<int>& y) {
+    std::vector<MNISTSample> dataset = loadMNISTCSV(path);
+    split_x_and_y(dataset, x, y);
 }
 
 std::vector<MNISTSample> loadMNISTCSV(const std::string& filename) {
@@ -61,12 +57,36 @@ std::vector<MNISTSample> loadMNISTCSV(const std::string& filename) {
         sample.label = std::stoi(value);
 
         while (std::getline(ss, value, ',')) {
-            sample.pixels.push_back(std::stod(value));
+            sample.pixels.push_back(std::stoi(value));
         }
 
         dataset.push_back(sample);
     }
 
     return dataset;
+}
+
+void split_x_and_y(std::vector<MNISTSample>& dataset, std::vector<std::vector<int>>& x, std::vector<int>& y) {
+    for (auto& s : dataset) {
+        x.push_back(s.pixels);
+        y.push_back(s.label);
+    }
+}
+
+void ReLU(std::vector<double>& arr) {
+    for (auto& val : arr) {
+        val = std::max(0.0, val);
+    }
+}
+
+void softmax(std::vector<double>& arr) {
+    double sum = 0.0;
+    for (auto& val : arr) {
+        val = std::pow(EULERS_NUMBER, val);
+        sum += val;
+    }
+    for (auto& val : arr) {
+        val /= sum;
+    }
 }
 
